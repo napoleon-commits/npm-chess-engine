0e9e10a57e7323d14eee7b39b7b810e76e1fd13d
Byron Hall <bjhall2@crimson.ua.edu>
2020-09-26 18:55:51 -0500

    added definitions

---
+++ src/components/Engine.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+      <header>
+          <h3>NPM Chess Engine</h3>
+      </header>
+      <div>
+          Fen:<br />
+          <input type="text"/>
+          <button type="button">
+              Set Position
+          </button>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    //   eslint-disable-next-line
+    console.log('mounted() called');
+  },
+};
+</script>
+
+<style>
+
+</style>
