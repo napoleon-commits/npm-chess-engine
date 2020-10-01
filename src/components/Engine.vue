6448e16af48ffcb4d2b84a522070e482395cbf03
Byron Hall <bjhall2@crimson.ua.edu>
2020-10-01 15:30:04 -0500

    gui

--- src/components/Engine.vue
+++ src/components/Engine.vue
@@ -1,8 +1,36 @@
 <template>
   <div>
+    <div>
       <span>Fen: </span>
       <input type="text" v-model="fenIn"/>
       <button @click="vueSetFen">Set Position</button>
+    </div>
+    <div>
+      TABLE
+    </div>
+    <div>
+      <span>Thinking Time:</span><br />
+      <select v-model="thinkingTime">
+        <option value="1">1s</option>
+        <option value="2">2s</option>
+        <option value="4">4s</option>
+        <option value="6">6s</option>
+        <option value="8">8s</option>
+        <option value="10">10s</option>
+      </select>
+      <br/><br/><br/>
+      <span>BestMove:</span><br />
+      <span>Depth:</span><br />
+      <span>Score:</span><br />
+      <span>Nodes:</span><br />
+      <span>Ordering:</span><br />
+      <span>Time:</span><br />
+      <button type="button">Move Now</button><br />
+      <button type="button">New Game</button><br />
+      <button type="button">Flip Board</button><br />
+      <button type="button">Take Back</button><br />
+      <span></span>
+    </div>
   </div>
 </template>
 
@@ -17,6 +45,7 @@ export default {
   data() {
     return {
       fenIn: '',
+      thinkingTime: '1',
     };
   },
   mounted() {
